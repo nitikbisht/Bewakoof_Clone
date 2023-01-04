730704fef03e89928edb3e64464a8eea26779fe7
nitikbisht <nitikbisht62@gmail.com>
2023-01-04 15:38:19 +0530

    products added

--- bewakoof/src/Components/MenClothing.js
+++ bewakoof/src/Components/MenClothing.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import data from "../db.json"
+import ProductFilter from './ProductFilter'
+import style from "./MenClothing.module.css"
+import Display from './Display'
+
 
 function MenClothing() {
+    console.log(data.men)
   return (
-    <div>MenClothing</div>
+    <div className={style.container}>
+        <div className={style.top}>
+        <h1>MEN CLOTHING </h1>
+        <h2></h2>
+        <span>({data.men.length})</span>
+        </div>
+        <div className={style.product}>
+            <ProductFilter/>
+            <Display/>
+        </div>
+
+    </div>
   )
 }
 
