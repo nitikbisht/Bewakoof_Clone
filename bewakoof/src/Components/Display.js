232e7e75ab9c2fd48b4d6fddc9b16f16f83f5818
nitikbisht <nitikbisht62@gmail.com>
2023-01-05 01:02:04 +0530

    products added

--- bewakoof/src/Components/Display.js
+++ bewakoof/src/Components/Display.js
@@ -1,8 +1,19 @@
 import React from 'react'
-
-function Display() {
+import style from "./display.module.css"
+function Display({data}) {
   return (
-    <div>Display</div>
+    <div className={style.display}>
+      <img src={data.image[0]} width={"100%"}/>
+      <div style={{display:"flex",justifyContent:"space-between"}}>
+        <h5>Bewakoof</h5>
+        <h6>icon</h6>
+      </div>
+      <p>{data.name}</p>
+      <div className={style.price}>
+        <h4>₹{data.price}</h4>
+        <h5>₹{data.originalprice}</h5>
+      </div>
+    </div>
   )
 }
 
