0c08ff759e6f0576cab6f82461545b6fe93618e2
nitikbisht <nitikbisht62@gmail.com>
2023-01-04 13:55:00 +0530

    products added

--- bewakoof/src/App.js
+++ bewakoof/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import MenClothing from "./Components/MenClothing"
+
+
 
 function App() {
   return (
     <div className="App">
       <Routes>
         {/* <Route path='/' element={}/> */}
+        <Route path='/men-clothing' element={<MenClothing/>}/>
       </Routes>
     </div>
   );
---
+++ bewakoof/src/Components/MenClothing.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MenClothing() {
+  return (
+    <div>MenClothing</div>
+  )
+}
+
+export default MenClothing
