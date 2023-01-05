cd2fe129309c4e78ee5c2df92a7d3bd96312668d
nitikbisht <nitikbisht62@gmail.com>
2023-01-05 15:24:49 +0530

    products added

--- bewakoof/src/Components/Productdescription.js
+++ bewakoof/src/Components/Productdescription.js
@@ -12,15 +12,34 @@ function Productdescription() {
     })
     const[state,setState]=useState(arr[0].image[0])
     console.log(arr)
+    let off=((arr[0].originalprice-arr[0].price)/arr[0].originalprice)*100
+    off=Math.round(off)
   return (
     <div className={style.container}>
-    <div  className={style.smallimg}>
-      {arr[0].image.map((el)=>{
-        return <img src={el} width={"55px"} onClick={()=>{setState(el)}}/>
-      })}
-    </div>
-      <div>
+      <div  className={style.smallimg}>
+        {arr[0].image.map((el,ind)=>{
+          return <img key={ind+1} src={el} width={"55px"} onClick={()=>{setState(el)}}/>
+        })}
+      </div>
+      <div className={style.bigimg}>
         <img src={state} width={"100%"} height={"500px"}/>
+      </div>
+      <div className={style.content}>
+        <div>
+          <h3>BEWAKOOF</h3>
+          <h3>{arr[0].name}</h3>
+        </div>
+        <div className={style.rating}>RATING</div>
+        <div>
+          <div className={style.price}>
+            <h3>₹{arr[0].price}</h3>
+            <h4 style={{textDecoration:"line-through",color:"#949494"}}>₹{arr[0].originalprice}</h4>
+            <h3 style={{color:"green"}}>{off}% OFF</h3>
+          </div>
+          <div>Inclusive of all Taxes</div>
+        </div>
+        <div></div>
+        <div></div>
       </div>
     </div>
   )
