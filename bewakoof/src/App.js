1beab8be6669b8e05a5dc3136ae7f11c74cfe1c7
himangshumahato82 <himangshumahato82@gmail.com>
2023-01-05 06:06:28 +0530

    mini carousel

--- bewakoof/src/App.js
+++ bewakoof/src/App.js
@@ -10,12 +10,49 @@ function App() {
       <div style={{display:"block",margin:"auto",padding:"20px"}}>
       <img src="https://images.bewakoof.com/uploads/grid/app/Desktop-Strip-1-1668681804.gif" alt="" />
       </div>
-       <div >
-        
+       <div  style={{width:"74%",height:"200px",display:"flex",margin:"auto"}}>
+         <div>
+           <img src="https://images.bewakoof.com/uploads/grid/app/DOTW-thumbnail-men-1672724845.gif" alt="" width={"120px"} />
+           <p style={{textAlign:'center',height:"5px",marginRight:"9px",marginLeft:"-8px",marginTop:"15px"}}>designs of the Week</p>
+         </div>
+         <div>
+         <img src="https://images.bewakoof.com/uploads/grid/app/Thumbnail-Bestseller-Men-1668491213.jpg" alt="" width={"120px"} />
+         <p style={{textAlign:'center',height:"5px",marginRight:"9px",marginLeft:"-10px",marginTop:"15px"}}>Bestsellers</p>
+         </div>
+         <div>
+         <img src="https://images.bewakoof.com/uploads/grid/app/hotdeals-2-1668491210.jpg" alt="" width={"120px"} />
+         <p style={{textAlign:'center',height:"5px",marginRight:"9px",marginLeft:"-10px",marginTop:"15px"}}>Hot Deals</p>
+         </div>
+         <div>
+         <img src="https://images.bewakoof.com/uploads/grid/app/Thumbnail-Customise-Men-1668491215.jpg" alt="" width={"120px"} />
+         <p style={{textAlign:'center',height:"5px",marginRight:"5px",marginTop:"15px"}}>Customization</p>
+         </div>
+         <div>
+         <img src="https://images.bewakoof.com/uploads/grid/app/Thumbnail-New-Arrivals-Men-1668491217.jpg" alt="" width={"120px"} />
+         <p style={{textAlign:'center',height:"5px",marginRight:"9px",marginLeft:"-10px",marginTop:"15px"}}>New Arrivals</p>
+         </div>
+         <div>
+         <img src="https://images.bewakoof.com/uploads/grid/app/last-size-new-thumbnaik-1668491212.jpg" alt="" width={"120px"} />
+         <p style={{textAlign:'center',height:"5px",marginRight:"0px",marginTop:"15px"}}>Last Sizes Left</p>
+         </div>
+         <div>
+         <img src="https://images.bewakoof.com/uploads/grid/app/Thumbnail-PlusSize-Men-1668491218.jpg" alt="" width={"120px"} />
+         <p style={{textAlign:'center',height:"5px",marginRight:"9px",marginLeft:"-10px",marginTop:"15px"}}>Plus Size</p>
+         </div>
+         <div>
+         <img src="https://images.bewakoof.com/uploads/grid/app/Thumbnail-Collabs-Men-1668491214.jpg" alt="" width={"120px"} />
+         <p style={{textAlign:'center',height:"5px",marginRight:"9px",marginLeft:"-10px",marginTop:"15px"}}>Official Collaboration</p>
+         </div>
+         <div>
+         <img src="https://images.bewakoof.com/uploads/grid/app/vote-thumbnail-1668491219.jpg" alt="" width={"120px"} />
+         <p style={{textAlign:'center',height:"5px",marginRight:"0px",marginTop:"15px"}}>Vote for Design</p>
+         </div>
        
        
        </div>
-
+          <div>
+          
+          </div>
 
 
     </div>
